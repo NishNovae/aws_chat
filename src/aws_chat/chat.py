d9832e5c5487d84f7a069398229ffb832e742926
NishNovae <raecrowned@gmail.com>
2024-08-22 11:32:45 +0900

    touchup

--- src/aws_chat/chat.py
+++ src/aws_chat/chat.py
@@ -2,29 +2,53 @@
 
 from kafka import KafkaProducer, KafkaConsumer
 from json import loads, dumps
-import time
 import threading
+
+#THREAD_RUNNING = True
+
+def create_data(username, message, end):
+    return {'sender': username, 'message': message, 'end': end}
 
 # SENDER
 def pchat(chatroom, username):
     sender = KafkaProducer(
-        # testing at localhost first
-#        bootstrap_servers = ['localhost:9092'],
         bootstrap_servers = ['ec2-43-203-182-252.ap-northeast-2.compute.amazonaws.com:9092'],
         value_serializer = lambda x: dumps(x).encode('utf-8'),
     )
 
-    while True:
-        message = ""
-        while message == "":
-            print(f"{username}: ", end="")
-            message = input()
-                
-        data = {'sender': username, 'message': message, 'time': time.time()}       
-#        print(f"[DEBUG] sender.send({chatroom}, value={data}")
-        sender.send(chatroom, value=data)
-        sender.flush()
-        time.sleep(0.5)
+    try:
+        initial_msg = f"User [{username}] has entered the chat!"
+        end = False
+
+#        data = {'sender': username, 'message': initial_msg, 'end': False}
+        data = create_data(username, initial_msg, end)
+        sender.send(chatroom, data)
+        sender.flush()        
+
+        while True:
+            message = ""
+            while message == "":
+                print(f"{username}: ", end="")
+                message = input()
+        
+            if message == 'exit':
+                message = f"User [{username}] has exited the chatroom."
+                end = True
+
+            data = {'sender': username, 'message': message, 'end': False}       
+            sender.send(chatroom, value=data)
+            sender.flush()
+
+            if end == True:
+                print("Exiting chat...")
+                sender.close()
+                return    
+
+    except KeyboardInterrupt:
+        print("Encountered keyboard interrupt. Finishing chat...")
+    finally:
+        sender.close()
+    return
 
     
 # RECEIVER
@@ -42,13 +66,20 @@ def cchat(chatroom, username):
     try:
         for message in receiver:
             data = message.value
-            if data['sender'] != username:
+            if data['end'] == True:
+                print()
+                print(f"User {data['sender']} has exited the chat.")
+                print("Type in 'exit' to also finish the chat.")
+                print(f"{username}: ", end="")
+
+            elif data['sender'] != username:
                 print()
                 print(f"{data['sender']}: {data['message']}")
                 print(f"{username}: ", end="")
 
     except KeyboardInterrupt:
-        print("Ending chat...")
+        print("Exiting chat...")
+        return
 
     finally:
         receiver.close()
@@ -67,3 +98,5 @@ thread_2 = threading.Thread(target = cchat, args = (chatroom, username))
 
 thread_1.start()
 thread_2.start()
+
+    
